da20880535a3564b295160c91609fadb7372ab4d
GuiManfrin134 <manfrin11gui@gmail.com>
2022-01-13 00:22:44 -0300

    Criando botões e aba de Favoritos

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
+import Favoritos from './pages/Favoritos';
 
 const Routex = () => {
     return(
@@ -11,6 +12,7 @@ const Routex = () => {
             <Routes>
                 <Route exact path="/" element={<Home />}/>
                 <Route exact path="/filme/:id" element={<Filme />}/>
+                <Route exact path="/favoritos" element={<Favoritos />}/>
             </Routes>
         </BrowserRouter>
     )
